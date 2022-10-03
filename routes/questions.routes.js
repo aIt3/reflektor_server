1d2a9a59d15c4b4cb4d0f6a790115a83a60eb8b0
alt3 <alt3@github.com>
2022-10-03 10:04:14 +0200

    date comparison doenst work

--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -4,13 +4,25 @@ const Question = require("../models/Question.model");
 const User = require('../models/User.model')
 
 //--------QUESTION ROUTES
-// POST /api/answers - Create a new Question 
+// POST /api/questions - Create a new Question 
 router.post('/questions', (req, res, next) => {
     const {question, date, questionType} = req.body
 
     Question.create({question, date, questionType})
         .then(response => res.json(response))
         .catch (err => res.json (err))
+
+})
+
+// GET /api/questions - Get the Question with the current Date 
+router.get('/questionsForDay', (req, res, next) => {
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const {question, date, questionType} = req.body
+
+    Question.find({date: {$gte: today}, question, date })
+    .then(response => res.json(response))
+    .catch (err => res.json (err))
 
 })
 
