5e732975f6af9c952721328d506c08e2642b3238
alt3 <alt3@github.com>
2022-10-03 11:42:42 +0200

    Delete and Edit Route created and tested

--- routes/answer.routes.js
+++ routes/answer.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { default: mongoose } = require("mongoose");
 const Answer = require('../models/Answer.model');
 const Question = require("../models/Question.model");
 const User = require('../models/User.model')
@@ -22,4 +23,32 @@ router.get('/allAnswersByUser', (req, res, next) => {
         .then(allAnswers => res.json(allAnswers))
         .catch(err => res.json(err))
 })
+// PUT /api/answers/:answerId, - Edit an specific Answer
+    router.put('/answers/:answerId', (req, res) =>{
+    const {answerId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(answerId)){
+        res.status(400).json({message: 'Specified id is not valid'})
+        return
+    }
+    const {answer, explanation} = req.body
+
+    Answer.findByIdAndUpdate(answerId, {answer, explanation}, {new:true}) 
+    .then(answer => res.json(answer))
+    .catch(err => console.log(err))
+})
+
+//DELETE / api/answers/:answerId
+
+router.delete('/answers/:answerId', (req, res) => {
+    const {answerId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(answerId)) {
+        res.status(400).json({message: 'Specific id is not valid'});
+        return
+    }
+    Answer.findByIdAndRemove(answerId)
+    .then(answer => res.json({message: `Project with the id ${answer._id} was succesfully deleted}`}))
+    .catch(err => console.log(err))
+})
+
 module.exports = router
